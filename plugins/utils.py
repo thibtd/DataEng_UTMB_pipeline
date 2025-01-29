a657e5656c6459c85c3136e8b9a67ddbc1519add
thibtd <thibautdonis1998@gmail.com>
2025-01-29 16:25:29 +0100

    recommender system

--- plugins/utils.py
+++ plugins/utils.py
@@ -5,9 +5,9 @@ def round_to_nearest_5(x:float)->float:
     x:float = float(x)
     return round(x/5)*5
 
-def get_lat_long(city:str)->tuple:
+def get_lat_long(place:str)->tuple:
     geolocator = Nominatim(user_agent="utmb_pipeline",timeout=40)
-    location = geolocator.geocode(city)
+    location = geolocator.geocode(place)
     return location.latitude, location.longitude
 
 def clean_dates(row:pd.Series)->tuple:
